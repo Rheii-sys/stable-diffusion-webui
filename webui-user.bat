bd8eabecab1ba9ff1ed7574b22f162e4f6a056da
Rheii-sys <rheiita45@gmail.com>
2024-08-15 23:07:36 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON=C:/Users/rheii/AppData/Local/Programs/Python/Python310/python.exe
+set GIT=C:/Users/rheii/AppData/Local/GitHubDesktop/GitHubDesktop.exe
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--skip-torch-cuda-test" after "set COMMANDLINE_ARGS=
 
 call webui.bat
